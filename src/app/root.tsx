8fa398b40596f3436df151719c8a932eacffd965
Azallio <allio.all.in.one@gmail.com>
2026-01-03 06:30:30 +0300

    feat: tanstask query, axios

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,11 +1,11 @@
 import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
 import type { Route } from './+types/root'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import './app.css'
+import { queryClient } from './query-client'
 
-export const links: Route.LinksFunction = () => [
-  { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' },
-]
+export const links: Route.LinksFunction = () => [{ rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }]
 
 export function Layout(props: React.PropsWithChildren) {
   const { children } = props
@@ -20,9 +20,11 @@ export function Layout(props: React.PropsWithChildren) {
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </QueryClientProvider>
       </body>
     </html>
   )
